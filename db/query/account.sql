fde6fdd70179d17e770a175bde2a1feef29c1223
hongzaitou <767067749@qq.com>
2023-10-18 01:18:32 +0800

    fix CRUD accounts and add CRUD entry and transfer

--- db/query/account.sql
+++ db/query/account.sql
@@ -12,19 +12,20 @@ SELECT *
 FROM accounts 
 WHERE id = $1 LIMIT 1;
 
--- name: ListAccount :one
+-- name: ListAccounts :many
 SELECT *
 FROM accounts
+ORDER BY id
 LIMIT $1
 OFFSET $2;
 
 -- name: UpdateAccount :one
 UPDATE accounts
 SET balance = $1
-WHERE id = $2;
+WHERE id = $2
 RETURNING *;
 
 -- name: DeleteAccount :exec
-DELETE FROM account
+DELETE FROM accounts
 WHERE id = $1;
 
