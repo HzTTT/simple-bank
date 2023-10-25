fbc09415393397194d27a57988275a1fb2ee1a23
hongzaitou <767067749@qq.com>
2023-10-26 02:13:20 +0800

    fix:403 forbiden for violate foreign key constrain

--- api/account.go
+++ api/account.go
@@ -6,6 +6,7 @@ import (
 
 	db "github.com/HzTTT/simple_bank/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
 )
 
 type createAccountRequest struct {
@@ -26,11 +27,18 @@ func (server *Server) createAccount(ctx *gin.Context) {
 		Currency: req.Currency,
 	}
 
-	account, err := server.store.CreateAccount(ctx,arg)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-		return
-	}
+	account, err := server.store.CreateAccount(ctx, arg)
+    if err != nil {
+        if pqErr, ok := err.(*pq.Error); ok {
+            switch pqErr.Code.Name() {
+            case "foreign_key_violation", "unique_violation":
+                ctx.JSON(http.StatusForbidden, errorResponse(err))
+                return
+            }
+        }
+        ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+        return
+    }
 
 	ctx.JSON(http.StatusOK,account)
 }
