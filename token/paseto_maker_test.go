63ea2fdb521621624e45a5e2d5a3098a8a963f87
hongzaitou <767067749@qq.com>
2023-10-28 00:19:04 +0800

    feat:add paseto maker and test

---
+++ token/paseto_maker_test.go
@@ -0,0 +1,65 @@
+package token
+
+import (
+	"testing"
+	"time"
+
+	"github.com/HzTTT/simple_bank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPasetoMaker(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	require.NoError(t, err)
+	require.NotEmpty(t, maker)
+
+	username := util.RandOwner()
+
+	token, err := maker.CreateToken(username, time.Minute)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, payload)
+
+	require.Equal(t, username, payload.Username)
+	require.NotZero(t, payload.ID)
+
+}
+
+func TestExpiredPasetoToken(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	token, err := maker.CreateToken(util.RandOwner(), -time.Minute)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrExpiredToken.Error())
+	require.Nil(t, payload)
+}
+
+/* func TestInvalidToken(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	require.NoError(t, err)
+	require.NotEmpty(t, maker)
+
+	username := util.RandOwner()
+
+	token, err := maker.CreateToken(username, time.Minute)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	token2, err := maker.CreateToken(util.RandOwner(), time.Minute)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token2)
+	require.NoError(t,err)
+	
+	require.NotEmpty(t, payload)
+}
+ */
