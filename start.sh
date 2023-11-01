4ce4bf291cf0f8d536a3424bdd57b8066375558c
hongzaitou <767067749@qq.com>
2023-11-01 17:46:15 +0800

    feat:add docker compose

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#the scrip will exit if a command return non-zero status
+set -e
+
+echo "run db migration"
+/app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
+
+echo "start the app"
+
+#take all the parameters passed to the scrip and run it
+echo "$@"
+exec "$@"
